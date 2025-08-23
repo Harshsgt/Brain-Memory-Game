cbf20e62aa2bfc32d0f82a2c40abb7b6bd387e4c
Harshsgt <harsh.trivedi847@gmail.com>
2025-08-23 17:27:41 +0530

    I add div and thier background images

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const parentDiv=document.querySelector("#card-section");
-for(let i=0; i<card-section.length; i++){
-    const childDiv=document.createElement("div")
-    childDiv.classList.add("card")
-    // childDiv.dataset.name=cardArray[i].name;
-    parenDiv.appendChild(childDiv)
+let cardsArray = [
+  { name: "CSS", 
+    img: "Assets/css logo.png" },
+  { name: "HTML", 
+    img: "Assets/Html logo.png" },
+  { name: "JQuery",
+     img: "Assets/jquery logo.png" },
+  { name: "JS", 
+    img: "Assets/js logo.png" },
+  { name: "Node", 
+    img: "Assets/node logo.png" },
+  { name: "Python",
+     img: "Assets/python logo.jpeg" },
+];
+
+const parentDiv = document.querySelector("#card-section");
+
+for (let i = 0; i < cardsArray.length; i++) {
+  const childDiv = document.createElement("div");
+  childDiv.classList.add("card");
+  childDiv.dataset.name = cardsArray[i].name;
+
+  childDiv.style.backgroundImage = `url("${cardsArray[i].img}")`;
+
+  parentDiv.appendChild(childDiv);
 }
