440aa0b2605f2a7a22a64e9b28e502c99242fe66
Harshsgt <harsh.trivedi847@gmail.com>
2025-08-23 14:44:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const parentDiv=document.querySelector("#card-section");
+for(let i=0; i<card-section.length; i++){
+    const childDiv=document.createElement("div")
+    childDiv.classList.add("card")
+    // childDiv.dataset.name=cardArray[i].name;
+    parenDiv.appendChild(childDiv)
+}
