aebc984eba91c1e2b29faf2182bf70af498f898f
Harshsgt <harsh.trivedi847@gmail.com>
2025-08-26 15:49:42 +0530

    Duplicate the cards

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 let cardsArray = [
-  { name: "CSS", 
-    img: "Assets/css logo.png" },
-  { name: "HTML", 
-    img: "Assets/Html logo.png" },
-  { name: "JQuery",
-     img: "Assets/jquery logo.png" },
-  { name: "JS", 
-    img: "Assets/js logo.png" },
-  { name: "Node", 
-    img: "Assets/node logo.png" },
-  { name: "Python",
-     img: "Assets/python logo.jpeg" },
+  { name: "CSS", img: "Assets/css logo.png" },
+  { name: "HTML", img: "Assets/Html logo.png" },
+  { name: "JQuery", img: "Assets/jquery logo.png" },
+  { name: "JS", img: "Assets/js logo.png" },
+  { name: "Node", img: "Assets/node logo.png" },
+  { name: "Python", img: "Assets/python logo.jpeg" },
 ];
 
 const parentDiv = document.querySelector("#card-section");
+const gameCard = cardsArray.concat(cardsArray);
+console.log(gameCard);
+let shuffledChild=Array.from(gameCard).sort(() => 0.5 - Math.random());
 
-for (let i = 0; i < cardsArray.length; i++) {
+
+for (let i = 0; i < shuffledChild.length; i++) {
   const childDiv = document.createElement("div");
   childDiv.classList.add("card");
-  childDiv.dataset.name = cardsArray[i].name;
+  childDiv.dataset.name = shuffledChild[i].name;
 
-  childDiv.style.backgroundImage = `url("${cardsArray[i].img}")`;
+  childDiv.style.backgroundImage = `url("${shuffledChild[i].img}")`;
 
   parentDiv.appendChild(childDiv);
 }
