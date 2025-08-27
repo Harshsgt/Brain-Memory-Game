246509f91ccdb48afee27d0de39c727ade53e702
Harshsgt <harsh.trivedi847@gmail.com>
2025-08-27 09:14:02 +0530

    add that at one time only two card select

--- app.js
+++ app.js
@@ -10,8 +10,35 @@ let cardsArray = [
 const parentDiv = document.querySelector("#card-section");
 const gameCard = cardsArray.concat(cardsArray);
 console.log(gameCard);
-let shuffledChild=Array.from(gameCard).sort(() => 0.5 - Math.random());
+let shuffledChild = Array.from(gameCard).sort(() => 0.5 - Math.random());
+let clickCount = 0;
+let firstCatd = "";
+let secondCard = "";
 
+parentDiv.addEventListener("click", (event) => {
+  let curCard = event.target;
+
+  if (curCard.id === "card-section") {
+    return false;
+  }
+
+  clickCount++;
+
+  if (clickCount < 3) {
+    if (clickCount === 1) {
+      firstCard = curCard.dataset.name;
+    } else {
+      secondCard = curCard.dataset.name;
+    }
+    if (firstCard !== "" && secondCard !== "") {
+        if(firstCard === secondCard){
+            curCard.classList.add('card_match')
+        }
+    }
+  }
+  curCard.classList.add("card_selected");
+});
+parentDiv.addEventListener;
 
 for (let i = 0; i < shuffledChild.length; i++) {
   const childDiv = document.createElement("div");
